b182fb053c0828933db2b7f8225a501cf3b20505
Pratyusha Vankayala <pratyushavankayala@gmail.com>
2018-05-18 13:53:02 -0700

    Random FortuneCookie Message

--- src/FortuneCookieMessage.kt
+++ src/FortuneCookieMessage.kt
@@ -26,19 +26,67 @@ In main(), print: "Your fortune is: ", followed by the fortune string.
 
 Extra practice:
 Use a for loop to run the program 10 times, or until the "Take it easy" fortune has been selected.
+
+Use the code you created in the above practice.
+
+The getFortune() function should really only be getting the fortune, and not be in the business of getting the birthday.
+
+Change your Fortune Cookie program as follows:
+
+Create a function called getBirthday() that gets the birthday from the user.
+Pass the result of getBirthday() to getFortune() using an Integer argument, and use it to return the correct fortune.
+Remove getting the birthday from getFortune()
+Instead of calculating the fortune based on the birthday, use a when statement to assign some fortunes as follows (or use your own conditions):
+If the birthday is 28 or 31...
+If the birthday is in the first week…
+else … return the calculated fortune as before.
+Hint: There are several ways in which to make this when statement. How much can you Kotlinize it?
 */
 
-fun main(args: Array<String>)  {
-    println(getFortuneCookie())
-}
-
-fun getFortuneCookie() : String {
-    var listOfFortuneCookieMessages : List<String> = listOf("You will have a great day!","Things will go well for you today.", "Enjoy a wonderful day of success.", "Be humble and all will turn out well."  )
-    print("Enter your birthday: ")
-    var birthday = readLine()
-
-    when (birthday?.toIntOrNull()) {
-        null -> return "null"
-        else -> return birthday?.div(listOfFortuneCookieMessages.size) ?: null
+fun main(args: Array<String>) {
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortune(getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break;
     }
 }
+
+fun getFortune(bdy : Int) : String {
+    var listOfFortuneCookieMessages : List<String> = listOf("Take it happy and enjoy life!", "You will have a great day!",
+            "Things will go well for you today.", "Enjoy a wonderful day of success.", "Take it easy", "Be humble and all will turn out well.")
+    return when(bdy) {
+        in 28..31 -> listOfFortuneCookieMessages[0]
+        in 1..7 -> listOfFortuneCookieMessages[1]
+        // bdy > 31 -> "invalid birthday"
+        else -> listOfFortuneCookieMessages[bdy.rem(listOfFortuneCookieMessages.size)]
+    }
+}
+
+fun getBirthday() : Int {
+    print("Enter your birthday: ")
+    var birthday = readLine()?.toIntOrNull() ?: 1
+    return birthday
+}
+
+/* fun getBirthday(): Int {
+    print("\nEnter your birthday: ")
+    return readLine()?.toIntOrNull() ?: 1
+}
+
+fun getFortune(birthday: Int): String {
+    val fortunes = listOf("You will have a great day!",
+            "Things will go well for you today.",
+            "Enjoy a wonderful day of success.",
+            "Be humble and all will turn out well.",
+            "Today is a good day for exercising restraint.",
+            "Take it easy and enjoy life!",
+            "Treasure your friends, because they are your greatest fortune.")
+    val index = when (birthday) {
+        in 1..7 -> 4
+        28, 31 -> 2
+        else -> birthday.rem(fortunes.size)
+    }
+    return fortunes[index]
+}
+*/
